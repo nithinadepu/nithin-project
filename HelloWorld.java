67d5fa52fadfc463fa8ed0e76b08589838c051fb
nithinadepu <nithin.adepu@gmail.com>
2025-09-22 15:41:53 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! welcome to Jenkins");
+        System.out.println("Hello, World! welcome to Jenkins, scm poll");
     }
 }
